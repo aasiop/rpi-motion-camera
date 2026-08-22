0f194530d0b7063a58efbbc963cf3da1dd84b616
aasiop <drugiasiop@gmail.com>
2026-08-22 12:37:01 +0200

    Added merge logs

--- merger.py
+++ merger.py
@@ -10,16 +10,25 @@ INPUT_DIR = os.path.join(os.getenv("PROJECT_PATH"), "recordings")
 OUTPUT_DIR = os.path.join(os.getenv("PROJECT_PATH"), "merged")
 TMP_DIR = os.getenv("TEMP_DIR")
 TMP_DIR_REC = os.path.join(os.getenv("PROJECT_PATH"), "recordings", ".tmp")
+LOG_DIR = os.path.join(os.getenv("PROJECT_PATH"), "logs")
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 os.makedirs(TMP_DIR, exist_ok=True)
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE = os.path.join(LOG_DIR, f"log_{datetime.now().strftime('%Y-%m-%d')}.txt")
+
+def log(message):
+    print(message)
+    with open(LOG_FILE, "a") as f:
+        f.write(message + "\n")
 
 #date in format YYYY-MM-DD
 yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
 
 groups = defaultdict(list)
 
-print(f"Finding files from: {yesterday}")
+log(f"Finding files from: {yesterday}")
 
 #load files
 for file in os.listdir(INPUT_DIR):
@@ -33,7 +42,7 @@ for file in os.listdir(INPUT_DIR):
     parts = name.rsplit("_", 2)
 
     if len(parts) != 3:
-        print(f"Skiping... (incorrect name): {file}")
+        log(f"Skiping... (incorrect name): {file}")
         continue
 
     _, date_part, _ = parts
@@ -45,7 +54,7 @@ for file in os.listdir(INPUT_DIR):
     groups[date_part].append(os.path.join(INPUT_DIR, file))
 
 if not groups:
-    print(f"No files from {yesterday}")
+    log(f"No files from {yesterday}")
 else:
     #Merge and delete
     for date, files in groups.items():
@@ -59,7 +68,7 @@ else:
 
         output_file = os.path.join(OUTPUT_DIR, f"merged_{date}.mp4")
 
-        print(f"Merging files from {date} ({len(files)})")
+        log(f"Merging files from {date} ({len(files)})")
 
         cmd = [
             "ffmpeg",
@@ -75,26 +84,26 @@ else:
 
         #Delete only after merging
         if result.returncode == 0:
-            print(f"Merged successfully. Deleting {len(files)} files.")
+            log(f"Merged successfully. Deleting {len(files)} files.")
 
             for file in files:
                 try:
                     os.remove(file)
                 except Exception as e:
-                    print(f"Can't delete {file}: {e}")
+                    log(f"Can't delete {file}: {e}")
         else:
-            print(f"ffmpeg error, day {date}.")
+            log(f"ffmpeg error, day {date}.")
 
 broken_files = os.listdir(TMP_DIR_REC)
 if len(broken_files) > 0:
-    print(f"Deleting {len(broken_files)} broken files in .tmp folder...")
+    log(f"Deleting {len(broken_files)} broken files in .tmp folder...")
     for file in broken_files:
         file_path = os.path.join(TMP_DIR_REC, file)
 
         try:
             os.remove(file_path)
-            print(f"Deleted: {file}")
+            log(f"Deleted: {file}")
         except Exception as e:
-            print(f"Can't delete {file_path}: {e}")
+            log(f"Can't delete {file_path}: {e}")
 
-print("DONE!")
+log("DONE!")
